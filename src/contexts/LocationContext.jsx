d39b789e20ae15fc3ff4495d1ce47a0462b1d2d1
Kuba-Nowoszynski <jakubnowoszynski@gmail.com>
2023-08-12 15:39:09 +0200

    change top level await

--- src/contexts/LocationContext.jsx
+++ src/contexts/LocationContext.jsx
@@ -12,7 +12,6 @@ async function fetchAPI() {
     return import.meta.env.VITE_OPEN_WEATHER_KEY;
   }
 }
-const API_KEY = await fetchAPI();
 
 export const LocationContext = createContext({
   location: "",
@@ -27,6 +26,7 @@ export const LocationProvider = ({ children }) => {
   const [forecast, setForecast] = useState([]);
 
   const searchLocation = async (search) => {
+    const API_KEY = await fetchAPI();
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${API_KEY}&units=metric`;
     const response = await axios.get(url); //get current weather data
     const data = response.data;
@@ -37,6 +37,7 @@ export const LocationProvider = ({ children }) => {
   };
 
   const searchByCoords = async (lat, lon) => {
+    const API_KEY = await fetchAPI();
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     const response = await axios.get(url); //get current weather data
     const data = response.data;
