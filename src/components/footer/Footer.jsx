2a4b81a2df50c3cfe58307abfa65dbe3a6a714ae
Kuba-Nowoszynski <jakubnowoszynski@gmail.com>
2023-08-14 03:42:25 +0200

    center footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./footer-styles.scss";
 export default function Footer({ haveData }) {
   return (
     <h6
-      className={`footer text-white-50 text-center pt-md-3 pt-lg-4 mt-lg-3 pt-5 mt-sm-5 pt-0 pb-0 mb-0 mt-0 ${
+      className={`text-white-50 text-center pt-md-3 pt-lg-4 mt-lg-3 pt-5 mt-sm-5 pt-0 pb-0 mb-0 mt-0  ${
         !haveData ? "bottom" : ""
       }`}
     >
